ab9a94e4f1f3971f010bccff552db82a656216bb
Mauricio David Castillo Ruvalcaba <211106@ids.upchiapas.edu.mx>
2024-07-17 13:49:48 -0600

    Initial setup of task management system with Vue 2, Nuxt.js, and Vuetify

---
+++ nuxt.config.js
@@ -0,0 +1,38 @@
+export default {
+    mode: 'universal',
+    head: {
+      title: 'Task Management System',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'My task management system' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    },
+    buildModules: [
+      '@nuxtjs/vuetify',
+    ],
+    vuetify: {
+      customVariables: ['~/assets/variables.scss'],
+      theme: {
+        dark: false,
+        themes: {
+          light: {
+            primary: '#1976D2',
+            secondary: '#424242',
+            accent: '#82B1FF',
+            error: '#FF5252',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FFC107'
+          }
+        }
+      }
+    },
+    build: {
+      extend(config, ctx) {}
+    }
+  }
+  
